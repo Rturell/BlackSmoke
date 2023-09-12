70faa30a1b73d14bf66c17f3ee2162032c27137f
rodrigoturell <rodrigoturell@gmail.com>
2023-09-11 21:29:42 -0300

    create js file for animations

---
+++ js/animations.js
@@ -0,0 +1,58 @@
+///SCROLL REVEAL /// 
+
+var sr = ScrollReveal({
+    duration: 2000,
+    mobile: false,
+
+});
+
+sr.reveal('.disc1', {
+    scale: 0.5,
+});
+
+sr.reveal('.title-album', {
+    origin: 'right',
+    distance: '100px',
+});
+
+sr.reveal('hr', {
+    origin: 'right',
+    distance: '100px',
+});
+
+sr.reveal('.text-album', {
+    origin: 'right',
+    distance: '100px',
+});
+
+sr.reveal('.formation', {
+    origin: 'right',
+    distance: '100px',
+});
+
+sr.reveal('.credits', {
+    origin: 'right',
+    distance: '100px',
+});
+
+sr.reveal('.icons', {
+    origin: 'right',
+    distance: '100px',
+});
+
+sr.reveal('.disc2', {
+    origin: 'right',
+    scale: 0.5,
+});
+
+sr.reveal('.title-album2', {
+    origin: 'left',
+    distance: '100px',
+});
+
+sr.reveal('.js-digital-btn', {
+    origin: 'left',
+    distance: '100px',
+});
+
+sr.reveal('.card', {});
